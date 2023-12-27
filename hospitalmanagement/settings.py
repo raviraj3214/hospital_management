b1968d0fb9429b16e5de11bc61e67685217461d8
raviraj3214 <raviraj32140@gmail.com>
2023-12-27 16:38:17 +0530

    error fix

--- hospitalmanagement/settings.py
+++ hospitalmanagement/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS=[STATIC_DIR,]
-STATIC_ROOT = BASE_DIR/ 'staticfiles',
+STATIC_ROOT = BASE_DIR/ 'staticfiles'
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'static')
 
