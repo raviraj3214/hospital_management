c609a2fde08b749762b1642402d8fa74a755bc64
raviraj3214 <raviraj32140@gmail.com>
2023-12-27 22:46:30 +0530

    dfghj

--- hospitalmanagement/settings.py
+++ hospitalmanagement/settings.py
@@ -121,12 +121,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 STATIC_ROOT = "/root/Hospital/static"
 
 # MEDIA_ROOT=os.path.join(BASE_DIR,'static')
-MEDIA_URL = "media/"
+MEDIA_URL = "/media/"
 
 # Directory where uploaded media files are stored
 MEDIA_ROOT = "/root/Hospital/media"
